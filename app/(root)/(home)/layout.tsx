872cdeb3c8ee93400eec01b0a1d94d00d48e976b
harsh3452 <harshpatil3452@gmail.com>
2025-03-27 18:50:36 +0530

    bugs fix

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,5 +1,5 @@
 import Navbar from '@/components/ui/Navbar';
-import  Sidebar  from '@/components/ui/Sidebar';
+import Sidebar from '@/components/ui/Sidebar';
 import { Metadata } from 'next';
 import React, { ReactNode } from 'react';
 
@@ -10,28 +10,21 @@ export const metadata: Metadata = {
     icon: '/icons/logo.svg'
   }
 };
-const HomeLayout = ({children}: {children : ReactNode}) => {
-    return (
-        <main className='relative'>
-         
-        <Navbar />
-
-         <div className='flex'>
-             
-              <Sidebar/>
-              
-              <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28
+const HomeLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className='relative'>
+      <Navbar />
+      <div className='flex'>
+        <Sidebar />
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28
               max-md:pb-14 sm:px-14">
-
-        <div className="w-full">
-
-         
-          {children}
+          <div className="w-full">
+            {children}
           </div>
-          </section>
-          </div>
-        </main>
-    );
+        </section>
+      </div>
+    </main>
+  );
 };
 
 export default HomeLayout;
